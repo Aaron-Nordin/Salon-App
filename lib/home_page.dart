ebe297fc8a93dc56fcd90223fab99d392d90cb4d
Aaron <nordin.aaron@gmail.com>
2023-05-15 12:29:05 -0600

    Added GridView to home_page and services_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:salon_app/learn_flutter_page.dart';
+// import 'package:salon_app/learn_flutter_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (BuildContext context) {
-                return const LearnFlutterPage();
-              },
-            ),
-          );
-        },
-        child: const Text('Learn Flutter'),
-      ),
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+      crossAxisCount: 3,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[100],
+          child: const Text("Temp 1"),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[200],
+          child: const Text('Temp 2'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[300],
+          child: const Text('Temp 3'),
+        ),
+      ],
     );
+    // return Center(
+    //   child: ElevatedButton(
+    //     onPressed: () {
+    //       Navigator.of(context).push(
+    //         MaterialPageRoute(
+    //           builder: (BuildContext context) {
+    //             return const LearnFlutterPage();
+    //           },
+    //         ),
+    //       );
+    //     },
+    //     child: const Text('Learn Flutter'),
+    //   ),
+    // );
   }
 }
--- lib/services_page.dart
+++ lib/services_page.dart
@@ -5,6 +5,34 @@ class ServicesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+      crossAxisCount: 2,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[100],
+          child: const Text("Temp 1"),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[200],
+          child: const Text('Temp 2'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[300],
+          child: const Text('Temp 3'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[400],
+          child: const Text('Temp 4'),
+        ),
+      ],
+    );
   }
 }
